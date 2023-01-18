99cfc50c015b5d89b7c29bb2a9a56b02c4db2cab
bangaradi <adityavb@cse.iitk.ac.in>
2023-01-19 02:41:50 +0530

    add update delete post

--- blog/blog/tests.py
+++ blog/blog/tests.py
@@ -36,7 +36,7 @@ class BlogTests(TestCase):
         self.assertTemplateUsed(response, 'home.html')
     
     def test_post_detail_view(self):
-        response = self.client.get('/post/1')
+        response = self.client.get('/post/1/')
         no_response = self.client.get('/post/100000/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
